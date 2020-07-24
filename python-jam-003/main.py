25f3098b3dd86c4685f449d8163ec376c5af9b4a
Avi Drucker <avidrucker@users.noreply.github.com>
2020-07-24 11:21:47 -0400

    Add files via upload

---
+++ python-jam-003/main.py
@@ -0,0 +1,18 @@
+print("Let's play!")
+
+# we invoke (use) the "input" function
+# which is built into Python
+myName = input("What's your name player 1?")
+
+# we concatenate together the words "welcome"
+# with the player's name
+print("Welcome " + myName)
+
+# question: How does one run Python
+# code/script inside of a Bash script?
+
+# question: In contrast to a "strongly typed"
+# language such as C#, how does Python assign
+# types to its variables? --> dynamically,
+# it's taken care of for you, you don't have to
+# specify
---
+++ python-jam-003/notes_001.py
@@ -0,0 +1,14 @@
+# we "invoke" (or call, use)
+# the print function with the
+# "argument" 'Hi everyone'
+print("Let's play TicTacToe")
+
+# example of how "escape a
+# character" 
+print('Let\'s play TicTacToe')
+
+# terminology
+# [] square brackets
+# () round parenthesis
+# {} curly brackets / braces
+# "character" any letter, number,symbol or whitespace that you  can type or see in a text document
